04639b279a29207c2cb5f2841752c062818e5991
nishun0419 <ninosekunlight@gmail.com>
2016-10-15 17:38:11 +0900

    test

---
+++ test return/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test return
+//
+//  Created by 二瀬征子 on 2016/08/27.
+//  Copyright © 2016年 二瀬征子. All rights reserved.
+import UIKit
+import MBCircularProgressBar
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBarView: MBCircularProgressBarView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+      override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
