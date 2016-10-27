801b66b0a90b2511127992679bebd52271d5ca91
nishun0419 <ninosekunlight@gmail.com>
2016-10-28 08:01:36 +0900

    commit3

--- test return/SetController.swift
+++ test return/SetController.swift
@@ -45,7 +45,81 @@ class SetController: UIViewController {
             priceField.text = "\(price)"
         }
     }
+    
+    @IBAction func tap9Button(_ sender: AnyObject) {
+        let value = priceField.text! + "9"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap4Button(_ sender: AnyObject) {
+        let value = priceField.text! + "4"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+        
+    }
+    
+    @IBAction func tap5Button(_ sender: AnyObject) {
+        let value = priceField.text! + "5"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap6Button(_ sender: AnyObject) {
+        let value = priceField.text! + "6"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap1Button(_ sender: AnyObject) {
+        let value = priceField.text! + "1"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap2Button(_ sender: AnyObject) {
+        let value = priceField.text! + "2"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap3Button(_ sender: AnyObject) {
+        let value = priceField.text! + "3"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap0Button(_ sender: AnyObject) {
+        let value = priceField.text! + "0"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap00Button(_ sender: AnyObject) {
+        let value = priceField.text! + "00"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+
+    }
+    
+    @IBAction func tapClearButton(_ sender: AnyObject) {
+        priceField.text! = "0"
+    }
+    
+    
 }
+
+    
+
     
         
 
